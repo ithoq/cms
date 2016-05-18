f7efeee08ebeda8ee3cda1e58f53aa6a2e0c1f60
Fabrice Cipolla <fcipolla@ttime.be>
2016-05-18 19:43:05 +0200

    comit

--- plugins/foo/src/main/java/be/ttime/modules/foo/repository/FooRepository.java
+++ plugins/foo/src/main/java/be/ttime/modules/foo/repository/FooRepository.java
@@ -1,6 +1,6 @@
 package be.ttime.modules.foo.repository;
 
-import be.ttime.core.persistence.model.Foo;
+import be.ttime.modules.foo.model.Foo;
 import org.springframework.data.repository.CrudRepository;
 
 public interface FooRepository extends CrudRepository<Foo, Long> {
