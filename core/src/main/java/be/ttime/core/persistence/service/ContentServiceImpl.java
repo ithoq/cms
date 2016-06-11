fd217048a054b5a8db455649e4b8da13991328e0
Petr KOKOREV <pkokorev@iterial.ch>
2016-06-11 18:33:57 +0200

    Compute slug in utils without reduction

--- core/src/main/java/be/ttime/core/persistence/service/ContentServiceImpl.java
+++ core/src/main/java/be/ttime/core/persistence/service/ContentServiceImpl.java
@@ -1,7 +1,11 @@
 package be.ttime.core.persistence.service;
 
 import be.ttime.core.error.ResourceNotFoundException;
-import be.ttime.core.persistence.model.*;
+import be.ttime.core.persistence.model.ContentDataEntity;
+import be.ttime.core.persistence.model.ContentEntity;
+import be.ttime.core.persistence.model.QContentDataEntity;
+import be.ttime.core.persistence.model.QContentEntity;
+import be.ttime.core.persistence.model.QTaxonomyTermEntity;
 import be.ttime.core.persistence.repository.IContentDataRepository;
 import be.ttime.core.persistence.repository.IContentRepository;
 import com.mysema.query.jpa.impl.JPAQuery;
@@ -13,8 +17,14 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import java.util.*;
+import javax.persistence.PersistenceContext;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+import java.util.Set;
+
+//import javax.persistence.EntityManagerFactory;
 
 @Service
 @Transactional
@@ -25,8 +35,11 @@ public class ContentServiceImpl implements IContentService {
     private IContentRepository contentRepository;
     @Autowired
     private IContentDataRepository contentDataRepository;
-    @Autowired
-    private EntityManagerFactory entityManagerFactory;
+//    @Autowired
+//    private EntityManagerFactory entityManagerFactory;
+
+    @PersistenceContext(unitName = "core")
+    private EntityManager entityManager;
 
     @Override
     @Cacheable(value = "content", key = "#slug")
@@ -37,31 +50,30 @@ public class ContentServiceImpl implements IContentService {
 
         QContentDataEntity contentDataEntity = QContentDataEntity.contentDataEntity;
         QContentEntity contentEntity = QContentEntity.contentEntity;
-        QTaxonomyTermEntity taxonomyTermDataEntity = QTaxonomyTermEntity.taxonomyTermEntity;
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
+//        QTaxonomyTermEntity taxonomyTermDataEntity = QTaxonomyTermEntity.taxonomyTermEntity;
         JPAQuery query = new JPAQuery(entityManager);
         ContentDataEntity result = query.from(contentDataEntity)
-             .leftJoin(contentDataEntity.dictionaryList).fetch()
-             .leftJoin(contentDataEntity.commentList).fetch()
-             .leftJoin(contentDataEntity.contentFiles).fetch()
-             .where(contentDataEntity.computedSlug.eq(slug).and(contentDataEntity.language.locale.eq(locale.toString())))
-             .singleResult(contentDataEntity);
-        entityManager.clear();
-        if(result != null) {
+                .leftJoin(contentDataEntity.dictionaryList).fetch()
+                .leftJoin(contentDataEntity.commentList).fetch()
+                .leftJoin(contentDataEntity.contentFiles).fetch()
+                .where(contentDataEntity.computedSlug.eq(slug).and(contentDataEntity.language.locale.eq(locale.toString())))
+                .singleResult(contentDataEntity);
+//        entityManager.clear();
+        if (result != null) {
             query = new JPAQuery(entityManager);
             ContentEntity parent = query.from(contentEntity)
-                                        .leftJoin(contentEntity.dataList, contentDataEntity).fetch()
-                                        .where(contentEntity.id.eq(result.getContent().getId()).and(contentDataEntity.language.locale.eq(locale.toString())))
-                                        .leftJoin(contentEntity.contentParent).fetch()
-                                        //.leftJoin(contentEntity.contentTemplate).fetch()
-                                        //.leftJoin(contentEntity.dictionaryList).fetch()
-                                        .leftJoin(contentEntity.privileges).fetch()
-                                        //.leftJoin(contentEntity.taxonomyTermEntities, taxonomyTermDataEntity).fetch()
-                                        //.leftJoin(taxonomyTermDataEntity.termDataList).fetch()
-                                        .singleResult(contentEntity);
+                    .leftJoin(contentEntity.dataList, contentDataEntity).fetch()
+                    .where(contentEntity.id.eq(result.getContent().getId()).and(contentDataEntity.language.locale.eq(locale.toString())))
+                    .leftJoin(contentEntity.contentParent).fetch()
+                            //.leftJoin(contentEntity.contentTemplate).fetch()
+                            //.leftJoin(contentEntity.dictionaryList).fetch()
+                    .leftJoin(contentEntity.privileges).fetch()
+                            //.leftJoin(contentEntity.taxonomyTermEntities, taxonomyTermDataEntity).fetch()
+                            //.leftJoin(taxonomyTermDataEntity.termDataList).fetch()
+                    .singleResult(contentEntity);
             // we add parent to the first result
             result.setContent(parent);
-            entityManager.close();
+//            entityManager.close();
 
         }
         return result;
@@ -93,19 +105,19 @@ public class ContentServiceImpl implements IContentService {
         QContentEntity contentEntity = QContentEntity.contentEntity;
         QContentDataEntity contentDataEntity = QContentDataEntity.contentDataEntity;
         QTaxonomyTermEntity taxonomyTermDataEntity = QTaxonomyTermEntity.taxonomyTermEntity;
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
+//        EntityManager entityManager = entityManagerFactory.createEntityManager();
         JPAQuery query = new JPAQuery(entityManager);
         ContentEntity result = query.from(contentEntity)
                 .leftJoin(contentEntity.dataList, contentDataEntity).fetch()
                 .where(contentEntity.id.eq(id))
                 .leftJoin(contentEntity.contentParent).fetch()
                 .leftJoin(contentEntity.contentTemplate).fetch()
-                //.leftJoin(contentEntity.dictionaryList).fetch()
+                        //.leftJoin(contentEntity.dictionaryList).fetch()
                 .leftJoin(contentEntity.privileges).fetch()
                 .leftJoin(contentEntity.taxonomyTermEntities, taxonomyTermDataEntity).fetch()
                 .leftJoin(taxonomyTermDataEntity.termDataList).fetch()
                 .singleResult(contentEntity);
-        entityManager.close();
+//        entityManager.close();
         return result;
     }
 
@@ -117,13 +129,13 @@ public class ContentServiceImpl implements IContentService {
     public ContentEntity findContentAndContentData(Long id, String locale) {
         QContentEntity contentEntity = QContentEntity.contentEntity;
         QContentDataEntity contentDataEntity = QContentDataEntity.contentDataEntity;
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
+//        EntityManager entityManager = entityManagerFactory.createEntityManager();
         JPAQuery query = new JPAQuery(entityManager);
         ContentEntity result = query.from(contentEntity)
                 .leftJoin(contentEntity.dataList, contentDataEntity).fetch()
                 .where(contentEntity.id.eq(id).and(contentDataEntity.language.locale.eq(locale)))
                 .singleResult(contentEntity);
-        entityManager.close();
+//        entityManager.close();
         return result;
     }
 
@@ -217,21 +229,21 @@ public class ContentServiceImpl implements IContentService {
     @Cacheable(value = "mainNav", key = "#lang")
     public String getNavMenu(String lang) {
         QContentEntity contentEntity = QContentEntity.contentEntity;
-        QContentDataEntity contentDataEntity = QContentDataEntity.contentDataEntity;
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
-        EntityManager entityManager2 = entityManagerFactory.createEntityManager();
+//        QContentDataEntity contentDataEntity = QContentDataEntity.contentDataEntity;
+//        EntityManager entityManager = entityManagerFactory.createEntityManager();
+//        EntityManager entityManager2 = entityManagerFactory.createEntityManager();
         JPAQuery query = new JPAQuery(entityManager);
-        List<ContentEntity> result =
-                query.from(contentEntity)
-                        .leftJoin(contentEntity.dataList, contentDataEntity).fetch()
-                        .leftJoin(contentEntity.privileges).fetch()
-                        .where(contentEntity.enabled.eq(true)
-                                .and(contentEntity.menuItem.eq(true))
-                                .and(contentDataEntity.language.locale.eq(lang))
-                                .and(contentDataEntity.computedSlug.isNotNull()))
-                        .list(contentEntity);
+//        List<ContentEntity> result =
+//                query.from(contentEntity)
+//                        .leftJoin(contentEntity.dataList, contentDataEntity).fetch()
+//                        .leftJoin(contentEntity.privileges).fetch()
+//                        .where(contentEntity.enabled.eq(true)
+//                                .and(contentEntity.menuItem.eq(true))
+//                                .and(contentDataEntity.language.locale.eq(lang))
+//                                .and(contentDataEntity.computedSlug.isNotNull()))
+//                        .list(contentEntity);
 
-        query = new JPAQuery(entityManager2);
+//        query = new JPAQuery(entityManager2);
         List<ContentEntity> roots =
                 query.from(contentEntity)
                         .where(contentEntity.enabled.eq(true)
@@ -246,8 +258,8 @@ public class ContentServiceImpl implements IContentService {
         sb.append("<ul class='main-menu' id='main-menu'>");
         buildNavMenu(roots, sb);
         sb.append("</ul>");
-        entityManager.close();
-        entityManager2.close();
+//        entityManager.close();
+//        entityManager2.close();
         return sb.toString();
     }
 
@@ -269,7 +281,7 @@ public class ContentServiceImpl implements IContentService {
 
         for (ContentEntity p : pages) {
             sb.append("<li>");
-            if(!p.getDataList().isEmpty()) {
+            if (!p.getDataList().isEmpty()) {
                 ContentDataEntity data = p.getDataList().iterator().next();
                 sb.append("<a href='");
                 sb.append(data.getComputedSlug());
