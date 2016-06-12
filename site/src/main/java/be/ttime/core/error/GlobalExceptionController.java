a4223c79022eebf123bf6e96c39b088ee30db158
Petr KOKOREV <pkokorev@iterial.ch>
2016-06-12 13:29:11 +0200

    Method with "instance of" removed

--- site/src/main/java/be/ttime/core/error/GlobalExceptionController.java
+++ site/src/main/java/be/ttime/core/error/GlobalExceptionController.java
@@ -1,13 +1,10 @@
 package be.ttime.core.error;
 
-import be.ttime.core.persistence.service.IApplicationService;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.CharEncoding;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.mail.MailAuthenticationException;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -27,99 +24,63 @@ class GlobalExceptionController {
 
     public final static String VIEW_GENERAL = "error/general";
     private final static String VIEW_404 = "error/notFound";
-    @Autowired
-    IApplicationService applicationService;
 
-    public static ModelAndView doResolveException(HttpServletRequest request, HttpServletResponse response, Exception ex) {
-        log.error(request.getRequestURI() + " - " + ex.getMessage(), ex);
+    private static ModelAndView handleException(final Exception e, HttpServletRequest request, final HttpServletResponse response, int sc, final String view, final String messageCode) {
+        log.error(request.getRequestURI() + " - " + e.getMessage(), e);
 
-        if (ex instanceof ResourceNotFoundException) {
-            return handleException(HttpServletResponse.SC_NOT_FOUND, request, response, VIEW_404, "error.notFound");
-        } else if (ex instanceof UserNotFoundException) {
-            return handleException(HttpServletResponse.SC_NOT_FOUND, request, response, VIEW_GENERAL, "error.auth.userNotFound");
-        } else if (ex instanceof UserAlreadyExistException) {
-            return handleException(HttpServletResponse.SC_NOT_FOUND, request, response, VIEW_GENERAL, "error.auth.userExist");
-        } else if (ex instanceof InvalidOldPasswordException) {
-            return handleException(HttpServletResponse.SC_NOT_FOUND, request, response, VIEW_GENERAL, "error.auth.invalidOldPassword");
-        } else if (ex instanceof MailAuthenticationException) {
-            return handleException(HttpServletResponse.SC_NOT_FOUND, request, response, VIEW_GENERAL, "error.mail.config");
-        } else if (response.getStatus() == 403) {
-            return handleException(HttpServletResponse.SC_FORBIDDEN, request, response, VIEW_GENERAL, "error.forbidden");
-        } else {
-            return handleException(response.getStatus(), request, response, VIEW_GENERAL, "error.general");
-        }
-    }
-
-    private static ModelAndView handleException(int sc, HttpServletRequest request, HttpServletResponse response, String view, String messageCode) {
         response.setCharacterEncoding(CharEncoding.UTF_8);
         response.setContentType(MediaType.TEXT_HTML_VALUE);
         response.setStatus(sc);
-        response.setStatus(response.getStatus());
-        ModelAndView mv = new ModelAndView(view);
+
+        final ModelAndView mv = new ModelAndView(view);
         mv.addObject("msg", messageCode);
         return mv;
     }
 
     @ExceptionHandler({CmsNotInstalledException.class})
-    public String notInstalled(HttpServletResponse response, HttpServletRequest request, Model model) {
-
-      /*  try {
-            Locale[] locales = Arrays.copyOfRange(Locale.getAvailableLocales(), 1, Locale.getAvailableLocales().length);
-            Arrays.sort(locales, (l1, l2) -> l1.getDisplayName().compareTo(l2.getDisplayName()));
-
-            List<Locale> result = new ArrayList<>();
-
-            // remove special locale
-            for (Locale locale : locales) {
-                if(locale.toString().length() <= 5){
-                    result.add(locale);
-                }
-            }
-
-            model.addAttribute("locales", result);
-            model.addAttribute("csrf", CmsUtils.getCsrfInput(request));
-            model.addAttribute("adminLocales", applicationService.getAdminlanguages());
-        }
-        catch(Exception e){
-            log.error(e.toString());
-        }*/
+    public String notInstalled() {
         return "redirect:/admin/install";
     }
 
     @ExceptionHandler({ResourceNotFoundException.class})
-    public ModelAndView notFound(HttpServletRequest request, HttpServletResponse response, ResourceNotFoundException ex) throws IOException {
-        return doResolveException(request, response, ex);
+    public ModelAndView notFound(HttpServletRequest request, HttpServletResponse response, ResourceNotFoundException e) throws IOException {
+        return handleException(e, request, response, HttpServletResponse.SC_NOT_FOUND, VIEW_404, "error.notFound");
     }
 
     @ExceptionHandler({UserNotFoundException.class})
-    public ModelAndView notFound(HttpServletRequest request, HttpServletResponse response, UserNotFoundException ex) throws IOException {
-        return doResolveException(request, response, ex);
-    }
-
-    @ExceptionHandler({InvalidOldPasswordException.class})
-    public ModelAndView invalidPassword(HttpServletRequest request, HttpServletResponse response, InvalidOldPasswordException ex) throws IOException {
-        return doResolveException(request, response, ex);
+    public ModelAndView notFound(HttpServletRequest request, HttpServletResponse response, UserNotFoundException e) throws IOException {
+        return handleException(e, request, response, HttpServletResponse.SC_NOT_FOUND, VIEW_GENERAL, "error.auth.userNotFound");
     }
 
     @ExceptionHandler({UserAlreadyExistException.class})
-    public ModelAndView userExist(HttpServletRequest request, HttpServletResponse response, UserAlreadyExistException ex) throws IOException {
-        return doResolveException(request, response, ex);
+    public ModelAndView userExist(HttpServletRequest request, HttpServletResponse response, UserAlreadyExistException e) throws IOException {
+        return handleException(e, request, response, HttpServletResponse.SC_CONFLICT, VIEW_GENERAL, "error.auth.userExist");
+    }
+
+    @ExceptionHandler({InvalidOldPasswordException.class})
+    public ModelAndView invalidPassword(HttpServletRequest request, HttpServletResponse response, InvalidOldPasswordException e) throws IOException {
+        return handleException(e, request, response, HttpServletResponse.SC_FORBIDDEN, VIEW_GENERAL, "error.auth.invalidOldPassword");
     }
 
     @ExceptionHandler({MailAuthenticationException.class})
-    public ModelAndView mailAuth(HttpServletRequest request, HttpServletResponse response, MailAuthenticationException ex) throws IOException {
-        return doResolveException(request, response, ex);
+    public ModelAndView mailAuth(HttpServletRequest request, HttpServletResponse response, MailAuthenticationException e) throws IOException {
+        return handleException(e, request, response, HttpServletResponse.SC_FORBIDDEN, VIEW_GENERAL, "error.mail.config");
     }
 
     @ExceptionHandler(PagePersistenceException.class)
     public ModelAndView pagePersistence(final HttpServletRequest request, final HttpServletResponse response, final PagePersistenceException e) throws Exception {
         log.error(e.getMessage(), e);
         final HttpStatus status = PagePersistenceException.class.getAnnotation(ResponseStatus.class).code();
-        return handleException(status.value(), request, response, e.viewName(), e.errorKey());
+        return handleException(e, request, response, status.value(), e.viewName, e.errorKey);
     }
 
     @ExceptionHandler(Exception.class)
-    public ModelAndView exception(HttpServletRequest request, HttpServletResponse response, Exception ex) {
-        return doResolveException(request, response, ex);
+    public ModelAndView exception(HttpServletRequest request, HttpServletResponse response, Exception e) {
+        if (response.getStatus() == HttpServletResponse.SC_FORBIDDEN) {
+            return handleException(e, request, response, HttpServletResponse.SC_FORBIDDEN, VIEW_GENERAL, "error.forbidden");
+        } else {
+            return handleException(e, request, response, response.getStatus(), VIEW_GENERAL, "error.general");
+        }
     }
+
 }
